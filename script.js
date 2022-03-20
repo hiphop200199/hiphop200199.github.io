974c37d0a2aea10be11c63e172c710b639f287db
hiphop200199 <97800613+hiphop200199@users.noreply.github.com>
2022-03-21 00:34:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var actor=document.getElementById("mrcold");
 var say=document.getElementById("words");
 var mybackground=document.body;
+var sneeze=new Audio("baby-sneeze.wav");
+var hahaha=new Audio("cartoon-laugh.wav");
+var slide=new Audio("vehicle-movement.wav");
+
+
+
 
 function res(){
-    actor.style.display="block";
+    actor.src="冷冷君.png";
+    actor.style.visibility="visible";
     actor.style.borderRadius="0";
     say.innerHTML="我是冷冷君!來跟我玩!";
     say.style.textAlign="center";
@@ -12,22 +19,75 @@ function res(){
     actor.style.width="244px";
     actor.style.height="413px";
     mybackground.style.backgroundImage="none";
+    actor.style.animationName="none";
+}
+function sli(){
+   actor.src="冷冷君.png";
+   actor.style.visibility="visible";
+   actor.style.animationName="slide";
+   actor.style.animationDuration="1.5s";
+   actor.style.animationTimingFunction="linear";
+   mybackground.style.backgroundImage="none";
+   slide.play();
+   
+}
+function hor(){
+    actor.src="horse.svg";
+    actor.style.width="344px";
+    actor.style.height="413px";
+    say.innerHTML="馬牽到阿拉伯是什麼?馬來西亞!";
+    say.style.fontSize="30px";
+    mybackground.style.backgroundImage="none";
+    actor.style.animationName="none";
+}
+function fak(){
+    actor.src="fake.jpg";
+    actor.style.visibility="visible";
+    actor.style.borderRadius="0";
+    actor.style.width="244px";
+    actor.style.height="413px";
+    say.innerHTML="";
+    mybackground.style.backgroundImage="none";
+    actor.style.animationName="none";
+    hahaha.play();
+}
+function rot(){
+    actor.src="冷冷君.png";
+    actor.style.visibility="visible";
+    actor.style.animationName="rotat";
+    actor.style.animationDuration="2s";
+    actor.style.animationTimingFunction="linear";
+    mybackground.style.backgroundImage="none";
 }
 function bli(){
     mybackground.style.backgroundImage="url(blizzard.gif)";
     mybackground.style.backgroundSize="cover";
     say.innerHTML="賣火柴..1盒20...";
-    actor.style.display="none";
+    actor.style.visibility="hidden";
+    actor.style.width="244px";
+    actor.style.height="413px";
+    actor.style.animationName="none";
+    sneeze.play();
 }
 function sml(){
+    actor.src="冷冷君.png";
+    actor.style.visibility="visible";
     actor.style.transition="width 2s,height 2s";
     actor.style.width="122px";
     actor.style.height="210px";
     say.innerHTML="我是小小冰";
     say.style.fontSize="15px";
+    actor.style.animationName="none";
+    mybackground.style.backgroundImage="none";
+   
 }
 function bal(){
+    actor.src="冷冷君.png";
+    actor.style.visibility="visible";
     actor.style.transition="border-radius 2s";
     actor.style.borderRadius="50%";
     say.innerHTML="QQQ";
+    actor.style.animationName="none";
+    mybackground.style.backgroundImage="none";
+    
 }
